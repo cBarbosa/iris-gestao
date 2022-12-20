a86f0c59bea342a6aecd8839dfb1e2232473c40c
Rodrigo V <rodrigovcalabria@hotmail.com>
2022-12-20 19:34:52 -0300

    Spinner na tabela de clientes

--- IrisGestao/IrisSpa/src/app/pages/client/client-listing/client-listing.component.ts
+++ IrisGestao/IrisSpa/src/app/pages/client/client-listing/client-listing.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { LazyLoadEvent } from 'primeng/api';
 import { first } from 'rxjs';
 import { ClientService } from 'src/app/shared/services/client.service';
 
@@ -14,9 +15,14 @@ export class ClientListingComponent {
 		{ label: 'Inativo', status: 'inativo' },
 	];
 
+	totalClientCount: number;
+	isLoadingClients = false;
+	first = 0;
+	rows = 10;
+
 	constructor(private router: Router, private clientService: ClientService) {}
 
-	clients = [
+	clientEntries = [
 		{
 			nome: 'Courtney Henry',
 			cpf_cnpj: '72.165.630/0001-27',
@@ -42,11 +48,11 @@ export class ClientListingComponent {
 			.subscribe((event: any) => {
 				console.log('>>>>>>>>>>>>>>>>>>>>', event);
 
-				this.clients = [];
+				this.clientEntries = [];
 
 				event.forEach((client: any) => {
 					console.log(client);
-					this.clients.push({
+					this.clientEntries.push({
 						name: client.nome,
 						cpf_cnpj: '72165630000127',
 						birthday: client.dataNascimento
@@ -58,6 +64,40 @@ export class ClientListingComponent {
 					});
 				});
 			});
+	}
+
+	loadClientsPage(event: LazyLoadEvent) {
+		if (event.first != null) {
+			this.isLoadingClients = true;
+			const page = Math.floor(event.first / this.rows) + 1;
+			this.getClientsPage(page);
+		}
+	}
+
+	getClientsPage(page = 1): void {
+		this.clientService
+			.getClients(this.rows, page)
+			?.pipe(first())
+			.subscribe((event: any) => {
+				if (event) {
+					this.totalClientCount = event.totalCount;
+					this.setClientEntries(event);
+				}
+				this.isLoadingClients = false;
+			});
+	}
+
+	setClientEntries({ items }: any) {
+		this.clientEntries = items.map((item: any) => {
+			return {
+				name: 'Courtney Henry',
+				cpf_cnpj: '72.165.630/0001-27',
+				birthday: new Date(),
+				client_type: 'Locatário',
+				status: 'inativo',
+				action: '',
+			};
+		});
 	}
 
 	navigateTo(route: string) {
