e50a814cc1be97af57bc24b1825bc329d03456f4
Charles Barbosa <xbrown@gmail.com>
2022-12-23 00:37:36 -0300

    Feat: get unit data by guid

--- IrisGestao/IrisApi/IrisInfra/Repository/Impl/UnidadeRepository.cs
+++ IrisGestao/IrisApi/IrisInfra/Repository/Impl/UnidadeRepository.cs
@@ -40,4 +40,73 @@ public class UnidadeRepository : Repository<Unidade>, IUnidadeRepository
 
         return lstUnidades.AsEnumerable();
     }
+    
+    public async Task<object?> GetByUid(Guid uid)
+    {
+        
+        return await DbSet
+            .Include(x => x.IdImovelNavigation)
+                .ThenInclude(y => y.IdCategoriaImovelNavigation)
+            .Include(x => x.IdImovelNavigation.IdClienteProprietarioNavigation)
+                .ThenInclude(z => z.IdTipoClienteNavigation)
+            .Include(x => x.IdTipoUnidadeNavigation)
+            .Select(y => new
+                {
+                    GuidReferencia = y.GuidReferencia,
+                    IdImovel = y.IdImovel,
+                    AreaUtil = y.AreaUtil,
+                    AreaTotal = y.AreaTotal,
+                    AreaHabitese = y.AreaHabitese,
+                    InscricaoIPTU = y.InscricaoIPTU,
+                    MatriculaEnergia = y.MatriculaEnergia,
+                    MatriculaAgua = y.MatriculaAgua,
+                    TaxaAdministracao = y.TaxaAdministracao,
+                    ValorPotencial = y.ValorPotencial,
+                    DataCriacao = y.DataCriacao,
+                    DataUltimaModificacao = y.DataUltimaModificacao,
+                    IdTipoUnidadeNavigation = new
+                    {
+                        Id = y.IdTipoUnidadeNavigation.Id,
+                        Nome = y.IdTipoUnidadeNavigation.Nome
+                    },
+                    IdImovelNavigation = new
+                    {
+                        GuidReferencia = y.IdImovelNavigation.GuidReferencia,
+                        Nome = y.IdImovelNavigation.Nome,
+                        AreaTotal = y.IdImovelNavigation.Unidade.Sum(x => x.AreaTotal),
+                        AreaUtil = y.IdImovelNavigation.Unidade.Sum(x => x.AreaUtil),
+                        AreaHabitese = y.IdImovelNavigation.Unidade.Sum(x => x.AreaHabitese),
+                        NroUnidades = y.IdImovelNavigation.Unidade.Count,
+                        ImgCapa = ImovelRepository.ImagemCapaFake,
+                        Imagens = ImovelRepository.ImagemListFake,
+                        Anexos = ImovelRepository.AnexoListFake,
+                        IdCategoriaImovelNavigation = new
+                        {
+                            Id = y.IdImovelNavigation.IdCategoriaImovelNavigation.Id,
+                            Nome = y.IdImovelNavigation.IdCategoriaImovelNavigation.Nome
+                        },
+                        IdClienteProprietarioNavigation = y.IdImovelNavigation.IdClienteProprietarioNavigation == null ? null : new 
+                        {
+                            GuidReferencia = y.IdImovelNavigation.IdClienteProprietarioNavigation.GuidReferencia,
+                            CpfCnpj = y.IdImovelNavigation.IdClienteProprietarioNavigation.CpfCnpj,
+                            Nome = y.IdImovelNavigation.IdClienteProprietarioNavigation.Nome,
+                            Telefone = y.IdImovelNavigation.IdClienteProprietarioNavigation.Telefone,
+                            Email = y.IdImovelNavigation.IdClienteProprietarioNavigation.Email,
+                            Cep = y.IdImovelNavigation.IdClienteProprietarioNavigation.Cep,
+                            Endereco = y.IdImovelNavigation.IdClienteProprietarioNavigation.Endereco,
+                            Bairro = y.IdImovelNavigation.IdClienteProprietarioNavigation.Bairro,
+                            Cidade = y.IdImovelNavigation.IdClienteProprietarioNavigation.Cidade,
+                            Estado = y.IdImovelNavigation.IdClienteProprietarioNavigation.Estado,
+                            IdTipoClienteNavigation = y.IdImovelNavigation.IdClienteProprietarioNavigation.IdTipoClienteNavigation == null
+                                ? null
+                                : new 
+                                {
+                                    Id = y.IdImovelNavigation.IdClienteProprietarioNavigation.IdTipoClienteNavigation.Id,
+                                    Nome = y.IdImovelNavigation.IdClienteProprietarioNavigation.IdTipoClienteNavigation.Nome,
+                                }
+                        }
+                    }
+                })
+            .FirstOrDefaultAsync(x => x.GuidReferencia.ToUpper().Equals(uid.ToString().ToUpper()));
+    }
 }
