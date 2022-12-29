f5aeb5c119353f820e142f5dfd16e23f83d9af45
Charles Barbosa <xbrown@gmail.com>
2022-12-29 10:58:15 -0300

    Feat: get address by zipcode

---
+++ IrisGestao/IrisApi/IrisAppService/Service/Interface/ExternalService/IRepublicaVirtualService.cs
@@ -0,0 +1,17 @@
+﻿namespace IrisGestao.Infraestructure.ExternalServices;
+
+public interface IRepublicaVirtualService
+{
+    Task<RepublicaVirtualResult> GetCepData(string cep);
+}
+
+public class RepublicaVirtualResult
+{
+    public string resultado { get; set; }
+    public string resultado_txt { get; set; }
+    public string uf { get; set; }
+    public string cidade { get; set; }
+    public string bairro { get; set; }
+    public string logradouro { get; set; }
+    public string tipo_logradouro { get; set; }
+}
