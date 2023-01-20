61e34bfd8f3d42ff251db09556026ebe8a9d6316
Rodrigo Calábria <rodrigovcalabria@hotmail.com>
2023-01-20 19:22:43 -0300

    manter contrato

--- IrisGestao/IrisSpa/src/app/shared/services/rent-contract.service.ts
+++ IrisGestao/IrisSpa/src/app/shared/services/rent-contract.service.ts
@@ -13,12 +13,15 @@ export class RentContractService {
 	getContracts(
 		limit: number = 50,
 		page: number = 1,
+		idTipo: number = 1,
 		filter?: string,
 		typeId?: number
 	) {
 		return this.http
 			.get<ApiResponse>(
-				`${env.config.apiUrl}Contrato?limit=${limit}&page=${page}${
+				`${
+					env.config.apiUrl
+				}ContratoAluguel?limit=${limit}&page=${page}&idTipo=${idTipo}${
 					filter ? `&nome=${filter}` : ''
 				}${typeId ? `&idTipo=${typeId}` : ''}`
 			)
@@ -27,6 +30,18 @@ export class RentContractService {
 					if (!response.success)
 						console.error(`getContracts: ${response.message}`);
 					return response;
+				})
+			);
+	}
+
+	getContractByGuid(guid: string) {
+		return this.http
+			.get<ApiResponse>(`${env.config.apiUrl}ContratoAluguel/${guid}/guid`)
+			.pipe(
+				map((response) => {
+					console.log('response', response);
+					if (response.success) return response.data;
+					else return console.error(`getContractByGuid: ${response.message}`);
 				})
 			);
 	}
