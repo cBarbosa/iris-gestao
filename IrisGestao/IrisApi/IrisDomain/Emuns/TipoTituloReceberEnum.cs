613d8d6368180988cd708f81b28298844cae1b86
Charles Barbosa <xbrown@gmail.com>
2023-04-25 12:59:48 -0300

    doc: namespace config

--- IrisGestao/IrisApi/IrisDomain/Emuns/TipoTituloReceberEnum.cs
+++ IrisGestao/IrisApi/IrisDomain/Emuns/TipoTituloReceberEnum.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace IrisGestao.Domain.Emuns;
 
-namespace IrisGestao.Domain.Emuns
+public static class TipoTituloReceberEnum
 {
-    public static class TipoTituloReceberEnum
-    {
-        public static int ALUGUEL = 2;
-    }
+    public static int ALUGUEL = 2;
 }
