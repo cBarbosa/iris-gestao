142006ce8acac469dde14df13ecce3417850dea6
Charles Barbosa <xbrown@gmail.com>
2023-04-30 18:46:44 -0300

    fix: auth path

--- IrisGestao/IrisSpa/src/environments/environment.prod.ts
+++ IrisGestao/IrisSpa/src/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
     config: {
         baseUrl,
         apiUrl: `${baseUrl}api/`,
-        b2cAuthUrl: 'https://irisgestaopatrimonial.b2clogin.com/irisgestaopatrimonial.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignUpSignIn&client_id=b3280fd3-bd17-46e9-a810-d06af7e0a632&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fpolite-beach-0ba132510.3.azurestaticapps.net%3A4200%2Fauthenticate&scope=openid&response_type=id_token&prompt=login&ui_locales=pt-BR'
+        b2cAuthUrl: 'https://irisgestaopatrimonial.b2clogin.com/irisgestaopatrimonial.onmicrosoft.com/oauth2/v2.0/authorize?p=B2C_1_SignUpSignIn&client_id=b3280fd3-bd17-46e9-a810-d06af7e0a632&nonce=defaultNonce&redirect_uri=https%3A%2F%2Fpolite-beach-0ba132510.3.azurestaticapps.net%2Fauthenticate&scope=openid%20https%3A%2F%2Firisgestaopatrimonial.onmicrosoft.com%2Fb3280fd3-bd17-46e9-a810-d06af7e0a632%2FAuth.Read&response_type=id_token%20token&prompt=login&ui_locales=pt-BR'
     }
 };
