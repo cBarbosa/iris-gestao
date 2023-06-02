00ede624646b219ccb7433f3365524228ab2789d
Charles Barbosa <xbrown@gmail.com>
2023-06-02 00:52:52 -0300

    feat: get financial vacancy dashboard data

---
+++ IrisGestao/IrisApi/IrisDomain/Command/Request/ConsultaDashboardCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace IrisGestao.Domain.Command.Request;
+
+public class ConsultaDashboardCommand
+{
+    public int? IdLocador { get; set; }
+    public int? IdTipoImovel { get; set; }
+    public DateTime? DateRefInit { get; set; }
+    public DateTime? DateRefEnd { get; set; }
+}
