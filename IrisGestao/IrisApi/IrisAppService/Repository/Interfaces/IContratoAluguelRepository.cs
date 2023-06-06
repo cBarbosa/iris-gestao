dbe981f814baf9916510094ca222d01b8670abaf
Charles Barbosa <xbrown@gmail.com>
2023-06-05 21:36:40 -0300

    feat: dashboard queries

--- IrisGestao/IrisApi/IrisAppService/Repository/Interfaces/IContratoAluguelRepository.cs
+++ IrisGestao/IrisApi/IrisAppService/Repository/Interfaces/IContratoAluguelRepository.cs
@@ -8,4 +8,8 @@ public interface IContratoAluguelRepository : IRepository<ContratoAluguel>, IDis
     Task<ContratoAluguel?> GetByGuid(Guid guid);
     Task<object?> GetByContratoAluguelGuid(Guid guid);
     Task<CommandPagingResult?> GetAllPaging(int? idTipoImovel, int? idBaseReajuste, DateTime? dthInicioVigencia, DateTime? dthFimVigencia, string? numeroContrato, int limit, int page);
+    Task<object> GetDashbaordFinancialVacancy(DateTime dateRefInit, DateTime dateRefEnd, int? idLocador, int? idTipoImovel);
+    Task<object> GetDashbaordPhysicalVacancy(DateTime dateRefInit, DateTime dateRefEnd, int? idLocador, int? idTipoImovel);
+    Task<object> GetDashboardTotalManagedArea(int? idLocador, int? idTipoImovel);
+    
 }
