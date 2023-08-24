4cb751e268d1a8faa9d91c8fb902c4c0a7611918
Renato Souza de Almeida <renato.s.almeida@outlook.com>
2023-08-24 00:35:23 -0300

    Ajuste melhorias feedback

--- IrisGestao/IrisApi/IrisDomain/Command/Request/CriarEventoCommand.cs
+++ IrisGestao/IrisApi/IrisDomain/Command/Request/CriarEventoCommand.cs
@@ -11,7 +11,7 @@ namespace IrisGestao.Domain.Command.Request
         public Guid GuidImovel { get; set; }
         public int? IdTipoEvento { get; set; }
         public Guid GuidCliente { get; set; }
-        public string TipoEvento { get; set; }
+        public string? TipoEvento { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public DateTime? DthRealizacao { get; set; }
