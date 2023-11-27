710b4b94111600f5f6a393c1d494c0af4eb467bb
Charles Barbosa <xbrown@gmail.com>
2023-11-26 22:29:56 -0300

    fix: revenue filter

--- IrisGestao/IrisApi/IrisAppService/Repository/Interfaces/ITituloPagarRepository.cs
+++ IrisGestao/IrisApi/IrisAppService/Repository/Interfaces/ITituloPagarRepository.cs
@@ -6,7 +6,7 @@ namespace IrisGestao.ApplicationService.Repository.Interfaces;
 public interface ITituloPagarRepository : IRepository<TituloPagar>, IDisposable
 {
     Task<int> GetNumeroTitulo();
-    Task<CommandPagingResult?> GetAllPaging(string? numeroTitulo, int? idTipoTitulo, int limit, int page);
+    Task<CommandPagingResult?> GetAllPaging(string? nomeProprietario, int? idTipoTitulo, int limit, int page);
     Task<TituloPagar?> GetByReferenceGuid(Guid guid);
     Task<TituloPagar?> GetByContratoAluguelId(int idContratoAluguel);
     Task<object?> GetByTituloPagarGuid(Guid guid);
